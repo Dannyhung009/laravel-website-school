9a6b90f384ca0cac92c881b400fd5d5cc569e846
dannyHung <khc741213@gmail.com>
2022-03-07 23:20:37 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+// Route::view('/', 'welcome');
+// Route::view('/home', 'home');
+Route::view('/', 'home');
+Route::view('/admin', 'admin');
